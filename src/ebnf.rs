891d8572cd6945de47e753bd90e2dd95fff77a22
Nagendra Kumar Jamadagni <nagendrajamadagni@gmail.com>
2025-04-21 10:12:56 -0500

    Added lexviz crate and parsed the ebnf microsyntax to be able to read ebnf files

--- src/ebnf.rs
+++ src/ebnf.rs
@@ -1,16 +1,14 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use color_eyre::{Report, Result};
+use lexviz::scanner::Token;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum RepetitionType {
     ZeroOrMore,
     OneOrMore,
     ZeroOrOne,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Term {
     Terminal(String),
     NonTerminal(String),
@@ -18,67 +16,110 @@ pub enum Term {
     Repetition(Box<Term>, RepetitionType),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Expression {
     sequence: Vec<Term>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Production {
     lhs: Term,
     rhs: Vec<Expression>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Grammar {
+    goal: Production,
     productions: Vec<Production>,
 }
 
+#[derive(Debug)]
+pub enum ParseError {
+    IncompleteProductionError,
+    MultipleLeftProductions(String, String),
+    InvalidProductionLHS(String),
+    UnbalancedParenError,
+    InvalidTokenErr(String),
+}
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::UnbalancedParenError => write!(f, "Error: Unbalanced parenthesis!"),
+            ParseError::IncompleteProductionError => {
+                write!(f, "Error: Production definition is incomplete!")
+            }
+            ParseError::InvalidProductionLHS(prod) => write!(
+                f,
+                "Error: {} is not a valid left side for a production!",
+                prod
+            ),
+            ParseError::InvalidTokenErr(token) => {
+                write!(f, "Error: {} is not a valid token!", token)
+            }
+            ParseError::MultipleLeftProductions(term1, term2) => write!(
+                f,
+                "Error: {} and {} two left hand terms found for a production!",
+                term1, term2
+            ),
+        }
+    }
+}
+
+impl std::error::Error for ParseError {}
+
 impl Expression {
-    fn parse(tokens: &Vec<(String, String)>, start: usize, end: usize) -> Self {
+    fn parse(tokens: &Vec<Token>, start: usize, end: usize) -> Result<Self> {
         let mut sequence: Vec<Term> = Vec::new();
 
         let mut idx = start;
 
         while idx <= end {
-            let word = &tokens[idx].0;
-            let category = &tokens[idx].1;
+            let word = &tokens[idx].get_token();
+            let category = &tokens[idx].get_category();
 
             let next_category = if idx == end {
                 None
             } else {
-                Some(&tokens[idx + 1].1)
+                Some(tokens[idx + 1].get_category())
             };
 
             let term = match category.as_str() {
-                "TERMINAL" => Term::Terminal(word.clone()),
-                "NON_TERMINAL" => Term::NonTerminal(word.clone()),
+                "TERMINAL" => Term::Terminal(word.to_string()),
+                "NON_TERMINAL" => Term::NonTerminal(word.to_string()),
                 "LPAREN" => {
-                    let mut depth = 1;
-                    let mut rparen_idx = idx + 1;
+                    let mut depth = 1; // Start at depth 1
+                    let mut rparen_idx = idx + 1; // Assume rparen is next token
 
                     while rparen_idx <= end && depth > 0 {
-                        if tokens[rparen_idx].1 == "LPAREN" {
-                            depth += 1;
-                        } else if tokens[rparen_idx].1 == "RPAREN" {
-                            depth -= 1;
+                        // While we are not at the end and the
+                        // depth is not 0
+                        if tokens[rparen_idx].get_category() == "LPAREN" {
+                            depth += 1; // If the token is an LPAREN increase the depth
+                        } else if tokens[rparen_idx].get_category() == "RPAREN" {
+                            depth -= 1; // If the token is an RPAREN decrease the depth
                         }
                         if depth == 0 {
+                            // If the depth is 0, exit
                             break;
                         }
-                        rparen_idx += 1;
+                        rparen_idx += 1; // Move the pointer to the next token
                     }
 
                     if depth != 0 {
-                        panic!("The parenthesis opened on line {} is never closed!", idx);
+                        let err = Report::new(ParseError::UnbalancedParenError);
+                        return Err(err);
                     }
 
-                    let inner_expression = Expression::parse(tokens, idx + 1, rparen_idx - 1);
+                    let inner_expression = Expression::parse(tokens, idx + 1, rparen_idx - 1)?;
                     idx = rparen_idx;
                     Term::Group(Box::new(inner_expression.sequence))
                 }
                 _ => {
-                    panic!("Invalid {} token found on line {}", tokens[idx].0, idx);
+                    let err = Report::new(ParseError::InvalidTokenErr(
+                        tokens[idx].get_token().to_string(),
+                    ));
+                    return Err(err);
                 }
             };
 
@@ -109,110 +150,88 @@ impl Expression {
             sequence.push(term);
         }
 
-        Expression { sequence }
+        Ok(Expression { sequence })
     }
 }
 
 impl Production {
-    fn parse(tokens: &Vec<(String, String)>, start: usize, end: usize) -> Self {
-        if tokens[start].1 != "NON_TERMINAL" {
-            panic!(
-                "Left hand side of production is not a non terminal symbol! Found {}",
-                tokens[start].0
-            );
+    fn parse(tokens: &Vec<Token>, start: usize, end: usize) -> Result<Self> {
+        if tokens[start].get_category() != "NON_TERMINAL" {
+            let err = Report::new(ParseError::InvalidProductionLHS(
+                tokens[start].get_token().to_string(),
+            ));
+            return Err(err);
         }
-        if tokens[start + 1].1 != "DEFINES" {
-            panic!(
-                "Left hand side of production has multiple terms! Found {} {}",
-                tokens[start].0,
-                tokens[start + 1].0
-            );
+        if tokens[start + 1].get_category() != "DEFINES" {
+            let err = Report::new(ParseError::MultipleLeftProductions(
+                tokens[start].get_token().to_string(),
+                tokens[start + 1].get_token().to_string(),
+            ));
+            return Err(err);
         }
 
-        let prod = &tokens[start].0;
-        let prod = &prod[1..prod.len() - 1];
+        let prod = &tokens[start].get_token();
 
         let lhs = Term::NonTerminal(prod.to_string());
 
         let mut rhs: Vec<Expression> = Vec::new();
 
-        let mut expression_start = start + 2;
+        let mut expression_start = start + 2; // Skip the defines
 
         for pos in expression_start..end {
-            if tokens[pos].1 == "ALTERNATION" {
-                let expression = Expression::parse(tokens, expression_start, pos - 1);
+            if tokens[pos].get_category() == "ALTERNATION" {
+                let expression = Expression::parse(tokens, expression_start, pos - 1)?;
+                // Parse everything until the alternation as a production rule
                 rhs.push(expression);
                 expression_start = pos + 1; // Consume the alternation itself
             }
         }
-        let expression = Expression::parse(tokens, expression_start, end);
+
+        // Parse the last production rule before the termination
+        let expression = Expression::parse(tokens, expression_start, end)?;
+
         rhs.push(expression);
 
-        Production { lhs, rhs }
+        Ok(Production { lhs, rhs })
     }
 }
 
 impl Grammar {
-    fn parse(file: File) -> Self {
-        let reader = BufReader::new(file);
+    fn parse(token_list: Vec<Token>) -> Result<Self> {
         let mut production_start = 0;
         let mut production_end = 0;
-        let mut lexed_lines: Vec<(String, String)> = Vec::new();
 
         let mut productions: Vec<Production> = Vec::new();
 
-        for (line_number, line) in reader.lines().enumerate() {
-            let content = match line {
-                Ok(content) => content,
-                Err(err) => panic!("Error reading line! {:?}", err),
-            };
+        for (token_number, token) in token_list.iter().enumerate() {
+            let category = token.get_category();
 
-            if !content.starts_with('(') || !content.ends_with(')') {
-                panic!("Invalid line provided! {}", content);
-            }
-
-            let content = &content[1..content.len() - 1];
-
-            if let Some(last_comma_index) = content.rfind(',') {
-                let word = &content[..last_comma_index].trim();
-                let category = &content[last_comma_index + 1..].trim();
-
-                lexed_lines.push((word.to_string(), category.to_string()));
-
-                if *category == "TERMINATION" {
-                    production_end = line_number;
-                    let production =
-                        Production::parse(&lexed_lines, production_start, production_end - 1);
-                    production_start = production_end + 1;
-                    productions.push(production);
-                }
-            } else {
-                panic!("Invalid line provided {}", content);
+            if category == "TERMINATION" {
+                production_end = token_number;
+                let production =
+                    Production::parse(&token_list, production_start, production_end - 1)?;
+                production_start = production_end + 1;
+                productions.push(production);
             }
         }
 
-        // If we reach end of file before completing a production print an error
-        if production_end != lexed_lines.len() - 1 {
-            panic!(
-                "The production started at line {} has not been terminated!",
-                production_start
-            );
+        // If we reach end of file before completing a production throw an error
+        if production_end != token_list.len() - 1 {
+            let err = Report::new(ParseError::IncompleteProductionError);
+            return Err(err);
         }
 
-        Grammar { productions }
+        Ok(Grammar {
+            goal: productions[0].clone(),
+            productions,
+        })
     }
 }
 
-pub fn parse_grammar(file_path: &str) -> Grammar {
-    let file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(err) => panic!(
-            "Could not open the provided file {:?}! {:?}",
-            file_path, err
-        ),
-    };
-    let parsed_grammar = Grammar::parse(file);
+/// Read an EBNF file and return the Grammar structure
+pub fn parse_grammar(token_list: Vec<Token>) -> Result<Grammar> {
+    let parsed_grammar = Grammar::parse(token_list)?;
     println!("The parsed grammar is {:?}", parsed_grammar);
 
-    return parsed_grammar;
+    return Ok(parsed_grammar);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
+use lexviz::{
+    construct_dfa, construct_minimal_dfa, construct_nfa, construct_scanner, parse_microsyntax_list,
+};
+
 mod ebnf;
 
+fn get_ebnf_mst_list() -> Vec<(String, String)> {
+    let mut ebnf_mst_list: Vec<(String, String)> = Vec::new();
+
+    ebnf_mst_list.push(("<[A-Za-z0-9-]+>".to_string(), "NON_TERMINAL".to_string()));
+    ebnf_mst_list.push((
+        "\"[A-Za-z0-9-:'!+<>=*()]+\"".to_string(),
+        "TERMINAL".to_string(),
+    ));
+    ebnf_mst_list.push(("::=".to_string(), "DEFINES".to_string()));
+    ebnf_mst_list.push(("\\|".to_string(), "ALTERNATION".to_string()));
+    ebnf_mst_list.push(("\\*".to_string(), "ASTERISK".to_string()));
+    ebnf_mst_list.push(("\\+".to_string(), "PLUS".to_string()));
+    ebnf_mst_list.push(("\\?".to_string(), "QUESTION".to_string()));
+    ebnf_mst_list.push(("\\(".to_string(), "LPAREN".to_string()));
+    ebnf_mst_list.push(("\\)".to_string(), "RPAREN".to_string()));
+    ebnf_mst_list.push((";".to_string(), "TERMINATION".to_string()));
+
+    return ebnf_mst_list;
+}
+
 fn main() {
-    let file_path = "diamondback.lex";
-    let diamondback_grammar = ebnf::parse_grammar(file_path);
+    let ebnf_mst_list = get_ebnf_mst_list();
+
+    let diamondback_ebnf = "diamondback.ebnf".to_string();
+
+    let syntax_tree_list = parse_microsyntax_list(ebnf_mst_list).unwrap();
+
+    let nfa = construct_nfa(syntax_tree_list, false).unwrap();
+
+    let dfa = construct_dfa(&nfa, false);
+
+    let minimal_dfa = construct_minimal_dfa(&dfa, false);
+
+    let scanner = construct_scanner(&minimal_dfa);
+
+    let lexed_output = scanner.scan(diamondback_ebnf, None, true, None).unwrap();
+
+    let diamondback_grammar = ebnf::parse_grammar(lexed_output).unwrap();
 }
